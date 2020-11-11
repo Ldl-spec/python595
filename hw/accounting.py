be0c0c1d7ffd626ad17b3aaadbb30339fc916e3c
Ldl-spec <70585414+Ldl-spec@users.noreply.github.com>
2020-11-11 11:47:05 +0800

    Update accounting.py

--- hw/accounting.py
+++ hw/accounting.py
@@ -52,12 +52,65 @@
 
 
 import sys
+import pandas as pd
+import numpy as np
 
 def parse_command_line(raw_command_line):
     return raw_command_line[1:]
 
 def parse_csv(filename):
-    raise NotImplementedError
+    '''
+    :param filename: The path to the incoming file
+    A third-party library is used pandas and numpy
+    Added two columns to the data, grouped in years and months,
+    and added income as a column
+    Turn the column name into a lesser case
+    '''
+
+    # Sets the number of outputs
+    number = 3
+
+    df = pd.read_csv(filename)
+
+    # Turn the column name into a lesser case
+    df.columns = df.columns.map(lambda x: x.lower())
+
+    # Read the file and sort it by time
+    df = df.sort_values("date")
+
+    # Add a column to the data (year, month)
+    temp_list = df["date"].str[:7].tolist()
+    df["date_tmp"] = pd.DataFrame(np.array(temp_list).reshape((df.shape[0], 1)))
+
+    # Get the quantity and price
+    quanTity = df["quantity"]
+    price = df["price"]
+
+    # Add a column of revenue to the data (revenue = quantity*price)
+    revenue = quanTity * price
+    df["total"] = pd.DataFrame(np.array(revenue).reshape((df.shape[0], 1)))
+
+    # Turn scientific notation into float
+    pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+    # total revenue
+    revenue_sum = revenue.sum()
+
+    # Group by year and month
+    grouped = df.groupby(by=df["date_tmp"]).sum()["total"].astype("float")
+
+    # get the most revenue
+    max_total = df[df["total"] == df["total"].max()]
+    print("*" * 50)
+    print("The name and total number of the largest earner{}  {}".format(max_total["clientname"].values, max_total["total"].values))
+    print("*" * 50)
+    print("Revenue {}".format(revenue_sum))
+    print("*" * 50)
+    print("Income from the top {}".format(number))
+    print(df["total"].sort_values(ascending=False).head(number).values)
+    print("*" * 50)
+    print("Income for the first {} months".format(number))
+    print(grouped.head(number))
 
 if __name__ == "__main__":
     files_to_parse = parse_command_line(sys.argv)
