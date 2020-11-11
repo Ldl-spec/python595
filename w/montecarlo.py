e2a6e5840352a71e00b8d7f911fed91b18c83306
Ldl-spec <70585414+Ldl-spec@users.noreply.github.com>
2020-11-11 11:37:15 +0800

    Update montecarlo.py

--- w/montecarlo.py
+++ w/montecarlo.py
@@ -1,1 +1,31 @@
+import random
 
+class NumericalIntegrator():
+
+    def __init__(self):
+        n = 0
+        m = 0
+
+    def integrate(self, lower_bound, upper_bound):
+
+        count = 0
+
+        x_min, x_max = lower_bound, self.m(upper_bound)
+        y_min, y_max = lower_bound, self.m(upper_bound)
+
+        for i in range(0, self.n):
+            x = random.uniform(x_min, x_max)
+            y = random.uniform(y_min, y_max)
+
+            if x * x > y:
+                count += 1
+
+        integral_value = count / float(self.n)
+        return integral_value
+
+
+    def set_objective(self, a):
+        self.m = a
+
+    def set_scenarios(self, n):
+        self.n = n
